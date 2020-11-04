da204aaf3277c1c03e3fea7faa721998522fbe32
Delia Constantino <dconstan92@gmail.com>
2020-11-04 19:01:17 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
   def count_sentences
     array = []
     self.split(/(\.|\?|!){1,}/).each do |string|
-      if string.include?("?") || string.include?(".") || string.include?("!") != true  
+      unless string.include?("?") || string.include?(".") || string.include?("!") 
         array << string
       end
     end
